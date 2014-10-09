efe7b971ad210682c3b03ed044f6d7044e50b11a
Daniel Loffgren <dloffgren@apple.com>
2014-10-08 17:58:25 -0700

    Initial import

---
+++ pyLoad/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  pyLoad
+//
+//  Created by Daniel Loffgren on 10/8/14.
+//  Copyright (c) 2014 Daniel Loffgren. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ pyLoad/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  pyLoad
+//
+//  Created by Daniel Loffgren on 10/8/14.
+//  Copyright (c) 2014 Daniel Loffgren. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+	// Insert code here to tear down your application
+}
+
+@end
