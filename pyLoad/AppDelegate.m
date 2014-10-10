6ef0008a68ab04987294dbb8e4d0049358af7b28
Daniel Loffgren <ryukojiro@gmail.com>
2014-10-09 19:15:46 -0700

    Support newDocument:

--- pyLoad/AppDelegate.m
+++ pyLoad/AppDelegate.m
@@ -10,24 +10,22 @@
 #import "MainWindowController.h"
 
 @interface AppDelegate ()
-
-@property (assign) IBOutlet MainWindowController *window;
 @end
 
 @implementation AppDelegate
 
-- (void)dealloc {
-	[_window release];
-	[super dealloc];
-}
-
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-	_window = [[MainWindowController alloc] initWithWindowNibName:@"MainWindowController"];
-	[_window showWindow:self];
+	[self newDocument:self];
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
 	// Insert code here to tear down your application
+}
+
+- (IBAction)newDocument:(id)sender {
+	MainWindowController *window = [[MainWindowController alloc] initWithWindowNibName:@"MainWindowController"];
+	[window showWindow:self];
+	// TODO: For now this leaks
 }
 
 @end
